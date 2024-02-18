2f5ebe62e8b4944ccd0e534bfa1f9739894354df
Kartofi <anatoli7707@gmail.com>
2024-02-18 21:27:07 +0200

    First web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use rouille;
+use rouille::router;
+
+use std::fs;
+
+
+fn main(){
+    rouille::start_server("0.0.0.0:8000", move |request| {
+        router!(request,
+            (GET) (/) => {
+                match fs::read_to_string("./src/views/index.html") {
+                    Ok(contents) => {
+                        // If successful, print the contents
+                        rouille::Response::html(contents)
+                    }
+                    Err(err) => {
+                        // If there's an error, print the error
+                        rouille::Response::text("ERROR!")
+                    }
+                }
+               
+            },
+            _ => rouille::Response::empty_404()
+        )
+    });
 }
