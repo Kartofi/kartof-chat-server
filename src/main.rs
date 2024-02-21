a050a117b4829adf14ecb8a5cb84cbbcd785661a
Kartofi <anatoli7707@gmail.com>
2024-02-21 22:55:16 +0200

    TCP Server USS LETS GO

--- src/main.rs
+++ src/main.rs
@@ -28,50 +28,16 @@ fn get_timestamp() -> u128 {
 const PORT:i64 = 8000;
 
 
-use std::thread;
-use std::net::{TcpListener, TcpStream, Shutdown};
-use std::io::{Read, Write};
 
+use ws::{connect, CloseCode, Message, Result};
 
-fn handle_client(mut stream: TcpStream) {
-    let mut data = [0 as u8; 50]; // using 50 byte buffer
-    while match stream.read(&mut data) {
-        Ok(size) => {
-            // echo everything!
-            stream.write(&data[0..size]).unwrap();
-            true
-        },
-        Err(_) => {
-            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
-            stream.shutdown(Shutdown::Both).unwrap();
-            false
-        }
-    } {}
-}
-
-
-
+use ws::listen;
 fn main(){
-    let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
-    // accept connections and process them, spawning a new thread for each one
-    println!("Server listening on port 3333");
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                thread::spawn(move|| {
-                    // connection succeeded
-                    handle_client(stream)
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                /* connection failed */
-            }
+    listen("127.0.0.1:3012", |out| {
+        move |msg| {
+            out.send(msg)
         }
-    }
-    // close the socket server
-    drop(listener);
+    });
     println!("Started listening on {}", PORT);
     rouille::start_server(format!("0.0.0.0:{}",PORT), move |request| {
         
